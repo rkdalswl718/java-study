1a300b6c36acc7083bdec15d54582efdd07c04ec
rkdalswl718 <rkdalswl070718@gmail.com>
2023-08-29 20:17:07 +0900

    2차원배열

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
-public class ValudeAssignment {
-    public static void  main(string[] args){}
-    int[] array1 = new int[3];
-    array[0] = 3;
-    array[1] = 4;
-    array[2] = 5;
-    System.out.println(array1[0] + " " + array[1] + " " + array[2]);
+public class Main{
+    public static void main(String[] args) {
+        int a[][] = new int[2][];
+        a[0] = new int[2];
+        a[0][0] = 1;
+        a[0][1] = 2;
 
-    int[] array2;
-    array2 = new int[3];
-    array2[0] = 3;
-    array2[1] = 4;
-    array2[2] = 5;
-    System.out.prinfln(array2[0] + array2[1] + " " + array[2]);
+        a[1] = new int[3];
+        a[1][0] = 3;
+        a[1][1] = 4;
+        a[1][2] = 5;
 
-    int[] array3 = new int[]{3,4,5};
-    System.out.println(array3[0] + " " + array[1] + " " + array3[2]);
-
-    int[] array4;
-    array4 = new int[]{3,4,5};
-    System.out.println(array5[0] + " " + array5[2]);
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.println(a[i][j]);
+            }
+        }
+    }
 }
