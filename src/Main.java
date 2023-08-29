b43eb4dac485fb7fd2bc2ac6a49c8053f03bf07a
rkdalswl718 <rkdalswl070718@gmail.com>
2023-08-29 19:46:14 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class ValudeAssignment {
+    public static void  main(string[] args){}
+    int[] array1 = new int[3];
+    array[0] = 3;
+    array[1] = 4;
+    array[2] = 5;
+    System.out.println(array1[0] + " " + array[1] + " " + array[2]);
+
+    int[] array2;
+    array2 = new int[3];
+    array2[0] = 3;
+    array2[1] = 4;
+    array2[2] = 5;
+    System.out.prinfln(array2[0] + array2[1] + " " + array[2]);
+
+    int[] array3 = new int[]{3,4,5};
+    System.out.println(array3[0] + " " + array[1] + " " + array3[2]);
+
+    int[] array4;
+    array4 = new int[]{3,4,5};
+    System.out.println(array5[0] + " " + array5[2]);
+}
