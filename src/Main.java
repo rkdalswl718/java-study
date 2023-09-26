3c9097212104e1bf64e35d086e52cd978de3bc6d
강민지 <128370710+rkdalswl718@users.noreply.github.com>
2023-09-26 20:33:16 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,37 @@
+package pack1;
+
+class A{}
+class B extends A {}
+
 public class Main {
-    public class A {
+    public static void main(String[] args) {
+        A aa = new A();
+        A ab = new B();
 
-        void printf() {
-            System.out.println("안녕");
+        System.out.println(ab instanceof A);
+        System.out.println(aa instanceof A);
+
+        System.out.println(aa instanceof B);
+        System.out.println(ab instanceof B);
+
+        if (aa instanceof B) {
+            B b = (B) aa;
+            System.out.println("aa를 B로 캐스팅함");
+        }
+        else {
+            System.out.println("aa는 B타입으로 캐스팅 불가능!!");
         }
 
-        public int main() { // 메인 메서드는 int 형식을 반환해야 합니다.
-            return 3;
+        if (ab instanceof  B) {
+            B b = (B) aa;
+            System.out.println("ab를 B로 캐스티앟ㅎㅁ");
+        }
+        else {
+            System.out.println("ab는 B타입으로 캐스팅 불가능");
         }
 
-        public double addNumbers(int a, double b) {
-            double sum = a + b;
-            return sum;
+        if ("안녕" instanceof String){
+            System.out.println("\"안녕\"은 String 클래스입니다");
         }
     }
 }
